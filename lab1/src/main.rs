3961d16ddfd255ae147f0d4b0204b5a2d894da9c
Wojciech Sęk <wojciech.sek17@gmail.com>
2023-04-13 00:24:05 +0200

    task 2 with H*

--- lab1/src/main.rs
+++ lab1/src/main.rs
@@ -11,7 +11,7 @@ fn str_to_bytes(s: &str) -> Vec<u8> {
     v   
 }
 
-fn verify(m0: &Vec<u8>, m1: &Vec<u8>, m0_p: &Vec<u8>, m1_p: &Vec<u8>) {
+fn verify(m0: &Vec<u8>, m1: &Vec<u8>, m0_p: &Vec<u8>, m1_p: &Vec<u8>) -> bool {
     let mut context = md5::Context::new();
     context.consume(m0);
     context.consume(m1);
@@ -22,11 +22,12 @@ fn verify(m0: &Vec<u8>, m1: &Vec<u8>, m0_p: &Vec<u8>, m1_p: &Vec<u8>) {
     context_p.consume(m1_p);
     let digest_p = context_p.compute();
     
-    assert_eq!(format!("{:x}", digest), format!("{:x}", digest_p));
-    print!("{:x}", digest_p);
+    println!("m  -> {:x}", digest_p);
+    println!("m' -> {:x}", digest_p);
+    digest == digest_p
 }
 
-fn main() {   
+fn task1() {
     let m0   = str_to_bytes("2dd31d1 c4eee6c5  69a3d69 5cf9af98 87b5ca2f ab7e4612 3e580440 897ffbb8
     634ad55  2b3f409 8388e483 5a417125 e8255108 9fc9cdf7 f2bd1dd9 5b3c3780");
     let m1   = str_to_bytes("d11d0b96 9c7b41dc f497d8e4 d555655a c79a7335  cfdebf0 66f12930 8fb109d1
@@ -35,5 +36,13 @@ fn main() {
     634ad55 2b3f409 8388e483 5a41f125 e8255108 9fc9cdf7 72bd1dd9 5b3c3780");
     let m1_p = str_to_bytes("d11d0b96 9c7b41dc f497d8e4 d555655a 479a7335 cfdebf0 66f12930 8fb109d1
     797f2775 eb5cd530 baade822 5c154c79 ddcb74ed 6dd3c55f 580a9bb1 e3a7cc35");
-    verify(&m0, &m1, &m0_p, &m1_p);
+    if verify(&m0, &m1, &m0_p, &m1_p) {
+        println!("Success!");
+    } else {
+        println!("Fail!");
+    }
+}
+
+fn main() {   
+    task1();
 }
